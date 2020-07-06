cd9ad9d0f2e214d4e92fc7bf0a181964ee95dbd6
조경진 <kj_cho@idealidea.co.kr>
2020-07-06 20:42:22 +0900

    네이버 웹링크 / 카카오 스토어링크

--- app/src/main/java/kr/co/tjoeun/androidintent_20200706/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/androidintent_20200706/MainActivity.kt
@@ -51,5 +51,18 @@ class MainActivity : AppCompatActivity() {
             startActivity(myIntent)
         }
 
+        naverWebLinkBtn.setOnClickListener {
+            val myUri = Uri.parse("https://naver.com")
+            val myIntent = Intent(Intent.ACTION_VIEW, myUri)
+            startActivity(myIntent)
+        }
+
+        kakaoPlayStoreBtn.setOnClickListener {
+            val myUri = Uri.parse("market://details?id=com.kakao.talk")
+            val myIntent = Intent(Intent.ACTION_VIEW, myUri)
+            startActivity(myIntent)
+        }
+
+
     }
 }
